454d8392c300d851fb71e527e0f05b19f3278c1c
SollentunaHC <gunnar@sollentunahockey.com>
2026-06-12 23:10:02 +0200

    Update swehockey.js

--- proxy/swehockey.js
+++ proxy/swehockey.js
@@ -1,1 +1,14 @@
+export async function onRequest(context) {
+  const url = "https://adm.swehockey.se/Feeds/Club.ashx?id=518";
+
+  const response = await fetch(url);
+  const xml = await response.text();
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+      "Access-Control-Allow-Origin": "*"
+    }
+  });
+}
 
